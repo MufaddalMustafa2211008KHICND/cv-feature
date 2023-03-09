8586dd1e5dbe1141ad7f64bc7e66e20cecb2dd20
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2023-03-09 16:35:03 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PdfComponent from './components/PdfComponent';
+import PdfFile from './components/PdfFile';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PDFDownloadLink document={<PdfFile />} filename="FORM">
+      {({loading}) => (loading ? <button>Loading Document...</button> : <button>Download</button> )}
+      </PDFDownloadLink>
+      {/* <div>
+        <PdfFile/>
+      </div> */}
     </div>
   );
 }
